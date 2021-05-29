774daf078ef2fca45d1d7f0daebbc470fd4721b5
Kevin Schaper <kevinschaper@gmail.com>
2021-05-29 00:09:28 -0700

    Xenbase genes, literature and phenotype are working - not too sure about the relation term for the gene2pub association

--- examples/xenbase/gene-literature.py
+++ examples/xenbase/gene-literature.py
@@ -2,7 +2,7 @@ import logging
 
 from koza.manager.data_provider import inject_row, inject_map, inject_translation_table
 from koza.manager.data_collector import collect
-from koza.model.biolink import Publication, Gene
+from koza.model.biolink import Publication, Gene, NamedThingToInformationContentEntityAssociation
 
 LOG = logging.getLogger(__name__)
 
@@ -10,7 +10,7 @@ source_name = 'gene-literature'
 
 row = inject_row(source_name)
 translation_table = inject_translation_table()
-genepage2gene = inject_map('genepage-to-gene')
+genepage2gene = inject_map('genepage-2-gene')
 
 entities = []
 
@@ -24,7 +24,7 @@ entities.append(publication)
 for gene_page in gene_pages.split(','):
     gene_page_id = gene_page.split(' ')[0]
     try:
-        gene_ids = genepage2gene[gene_page_id]
+        gene_ids = map(lambda id: f"Xenbase:{id}", list(genepage2gene[gene_page_id].values()))
     except KeyError:
         LOG.debug(f"Could not locate genepage_id: {gene_page_id} in row {row}")
         continue
@@ -32,9 +32,14 @@ for gene_page in gene_pages.split(','):
         gene = Gene()
         gene.id = gene_id
 
-        # TODO: publications seem to have relationships to associations, not sure what association
-        # TODO: to use to simply say that the publication mentions the gene
-
         entities.append(gene)
 
-collect(entities)
+        association = NamedThingToInformationContentEntityAssociation()
+        association.subject = gene.id
+        association.predicate = "IAO:0000142"  # Mentions
+        association.object = publication.id
+        association.relation = "RO:0001019"  # Contains todo: confirm this!
+
+        entities.append(association)
+
+collect(source_name, *entities)
--- examples/xenbase/gene-to-phenotype.py
+++ examples/xenbase/gene-to-phenotype.py
@@ -10,7 +10,6 @@ translation_table = inject_translation_table()
 gene = Gene()
 gene.id = 'Xenbase:' + row['SUBJECT']
 
-
 phenotype = PhenotypicFeature()
 phenotype.id = row['OBJECT']
 
