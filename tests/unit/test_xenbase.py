b9cdbf9ba8b2d11d94166b3a2c5f076436d1719d
Kevin Schaper <kevinschaper@gmail.com>
2021-07-07 18:55:53 -0700

    Tweaked slightly to reduce per-test code (need to figure out hiding in a fixture), fixed gene_literature test

--- tests/unit/test_xenbase.py
+++ tests/unit/test_xenbase.py
@@ -14,7 +14,7 @@ from koza.koza_runner import set_koza
 def mock_write(self, source_name, *entities):
     self._entities = list(*entities)
 
-def make_mock_koza_app(name:str, data: Iterable, transform_code: str, map=None, filters=None):
+def make_mock_koza_app(name:str, data: Iterable, transform_code: str, map_cache=None, filters=None, translation_table=None):
     mock_source_file_config = PrimaryFileConfig(
         name = name,
         files = [],
@@ -26,19 +26,24 @@ def make_mock_koza_app(name:str, data: Iterable, transform_code: str, map=None, 
     mock_source = Source(source_files=[mock_source_file])
 
     koza = KozaApp(mock_source)
-    # TODO impl map and filter mocks
+    # TODO filter mocks
+    koza.source.translation_table = translation_table
+    koza.map_cache = map_cache
     koza.write = types.MethodType(mock_write, koza)
 
     return koza
 
 
+def transform(name :str, data: Iterable, transform_code: str, map_cache=None, filters=None, translation_table=None):
+    koza_app = make_mock_koza_app(name, data, transform_code, map_cache=map_cache, filters=filters, translation_table=translation_table)
+    set_koza(koza_app)
+    koza_app.process_sources()
+    return koza_app._entities
 
 def test_gene_information_transform():
     row = iter([{'DB': 'Xenbase', 'DB_Object_ID': 'XB-GENE-478054', 'DB_Object_Symbol': 'trnt1', 'DB_Object_Name': 'tRNA nucleotidyl transferase, CCA-adding, 1', 'DB_Object_Synonym(s)': 'cca1|mtcca|tRNA nucleotidyl transferase|xcca', 'DB_Object_Type': 'gene', 'Taxon': 'taxon:8364', 'Parent_Object_ID': '', 'DB_Xref(s)': 'NCBI_Gene:394602|UniProtKB:F6V8Z1|UniProtKB:F7DM70|UniProtKB:Q6P873', 'Properties': ''}])
-    koza_app = make_mock_koza_app('gene-information', row, './examples/xenbase/gene-information.py')
-    set_koza(koza_app)
-    koza_app.process_sources()
-    entities = koza_app._entities
+
+    entities = transform('gene-information', row, './examples/xenbase/gene-information.py')
 
     assert len(entities) == 1
     gene = entities[0]
@@ -50,10 +55,7 @@ def test_gene_information_transform():
 
 def test_gene2_phenotype_transform():
     row = iter([{'SUBJECT': 'XB-GENE-1000632', 'SUBJECT_LABEL': 'dctn2', 'SUBJECT_TAXON': 'NCBITaxon:8364', 'SUBJECT_TAXON_LABEL': 'Xla', 'OBJECT': 'XPO:0102358', 'OBJECT_LABEL': 'abnormal tail morphology', 'RELATION': 'RO_0002200', 'RELATION_LABEL': 'has_phenotype', 'EVIDENCE': '', 'EVIDENCE_LABEL': '', 'SOURCE': 'PMID:17112317', 'IS_DEFINED_BY': '', 'QUALIFIER': ''}])
-    koza_app = make_mock_koza_app('gene-to-phenotype', row, './examples/xenbase/gene-to-phenotype.py')
-    set_koza(koza_app)
-    koza_app.process_sources()
-    entities = koza_app._entities
+    entities = transform('gene-to-phenotype', row, './examples/xenbase/gene-to-phenotype.py')
 
     assert(entities)
     assert(len(entities) == 3)
@@ -63,12 +65,12 @@ def test_gene2_phenotype_transform():
     assert len(genes) == 1
     assert len(phenotypes) == 1
     assert len(associations) == 1
-    #assert 'PMID:17112317' in associations[0].publications
+    assert 'PMID:17112317' in associations[0].publications
 
 
 def test_gene_literature_transform():
-    row = {'xb_article': 'XB-ART-1', 'pmid': '16938438', 'gene_pages': 'XB-GENEPAGE-487723 nog,XB-GENEPAGE-490377 msx1,XB-GENEPAGE-481828 bmp2,XB-GENEPAGE-483057 bmp4,XB-GENEPAGE-480982 fgf8,XB-GENEPAGE-6045068 hspa1l,XB-GENEPAGE-485625 hsp70'}
-    maps = {
+    row = iter([{'xb_article': 'XB-ART-1', 'pmid': '16938438', 'gene_pages': 'XB-GENEPAGE-487723 nog,XB-GENEPAGE-490377 msx1,XB-GENEPAGE-481828 bmp2,XB-GENEPAGE-483057 bmp4,XB-GENEPAGE-480982 fgf8,XB-GENEPAGE-6045068 hspa1l,XB-GENEPAGE-485625 hsp70'}])
+    map_cache = {
         'genepage-2-gene': {
             'XB-GENEPAGE-487723': {'tropicalis_id': 'XB-GENE-487724', 'laevis_l_id': 'XB-GENE-864891', 'laevis_s_id': 'XB-GENE-17332089'},
             'XB-GENEPAGE-490377': {'tropicalis_id': 'XB-GENE-490378', 'laevis_l_id': 'XB-GENE-490382', 'laevis_s_id': 'XB-GENE-6253888'},
@@ -80,7 +82,9 @@ def test_gene_literature_transform():
         }
     }
     tt = get_translation_table("examples/translation_table.yaml", None)
-    entities = gene_literature_transform(row, tt, maps)
+
+    entities = transform('gene-literature', row, './examples/xenbase/gene-literature.py', map_cache=map_cache, translation_table=tt)
+
     assert entities
     publications = [entity for entity in entities if isinstance(entity, Publication)]
     genes = [entity for entity in entities if isinstance(entity, Gene)]
