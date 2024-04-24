bc0c5028ec8b97effd5b072bc72a0c26d7908f54
glass-ships <glass.ships@outlook.com>
2024-04-24 17:10:52 -0600

    ok fixture works. removing class

--- src/koza/utils/testing_utils.py
+++ src/koza/utils/testing_utils.py
@@ -1,8 +1,8 @@
 import types
 from typing import Iterable
 
-from loguru import logger
 import pytest
+from loguru import logger
 
 from koza.app import KozaApp
 from koza.cli_utils import get_koza_app, get_translation_table, _set_koza_app
@@ -14,6 +14,7 @@ def test_koza(koza: KozaApp):
     """Manually sets KozaApp for testing"""
     global koza_app
     koza_app = koza
+
 
 @pytest.fixture(scope="package")
 def mock_koza():
@@ -80,63 +81,3 @@ def mock_koza():
         return koza_app._entities
 
     return _transform
-
-
-class MockKoza:
-    """Mock KozaApp for testing"""
-
-    def __init__(
-        self,
-        name: str,
-        data: Iterable,
-        transform_code: str,
-        map_cache=None,
-        filters=None,
-        global_table=None,
-        local_table=None,
-    ):
-        self.name = name
-        self.data = data
-        self.transform_code = transform_code
-        self.map_cache = map_cache
-        self.filters = filters
-        self.global_table = global_table
-        self.local_table = local_table
-
-        # return self.transform()
-
-    def mock_write(self, *entities):
-        if hasattr(self, "_entities"):
-            self._entities.extend(list(entities))
-        else:
-            self._entities = list(entities)
-
-    def make_mock_koza_app(self):
-        mock_source_file_config = PrimaryFileConfig(
-            name=self.name,
-            files=[],
-            transform_code=self.transform_code,
-        )
-        mock_source_file = Source(mock_source_file_config)
-        mock_source_file._reader = self.data
-
-        _set_koza_app(
-            source=mock_source_file,
-            translation_table=get_translation_table(self.global_table, self.local_table, logger),
-            logger=logger,
-        )
-        koza = get_koza_app(self.name)
-
-        # TODO filter mocks
-        koza._map_cache = self.map_cache
-        koza.write = types.MethodType(self.mock_write, koza)
-
-        return koza
-
-    def transform(self):
-        koza_app = self.make_mock_koza_app()
-        test_koza(koza_app)
-        koza_app.process_sources()
-        if not hasattr(koza_app, "_entities"):
-            koza_app._entities = []
-        return koza_app._entities
