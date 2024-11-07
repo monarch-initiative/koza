275f9912119d03d441bd83f00d66c29256ab0623
amc-corey-cox <69321580+amc-corey-cox@users.noreply.github.com>
2024-11-07 13:58:58 -0600

    Add extra parameter check to writer

--- src/koza/io/writer/writer.py
+++ src/koza/io/writer/writer.py
@@ -1,6 +1,7 @@
 from abc import ABC, abstractmethod
+from functools import lru_cache
 from pathlib import Path
-from typing import Iterable, List, Union
+from typing import Iterable, List, Union, Tuple
 
 from koza.converter.kgx_converter import KGXConverter
 from koza.model.config.sssom_config import SSSOMConfig
@@ -26,18 +27,33 @@ class KozaWriter(ABC):
 
         self.converter = KGXConverter()
 
-    def write(self, entities: Iterable):
+    def write(self, entities: Iterable, skip_checks: bool = False):
         nodes, edges = self.converter.convert(entities)
 
         if nodes:
             for node in nodes:
+                self.check_extra_fields(tuple(node.keys()), tuple(self.node_columns))
                 self.write_node(node)
 
         if edges:
             for edge in edges:
                 if self.sssom_config:
                     edge = self.sssom_config.apply_mapping(edge)
+                self.check_extra_fields(tuple(edge.keys()), tuple(self.edge_columns))
                 self.write_edge(edge)
+
+    @staticmethod
+    @lru_cache(maxsize=None)
+    def check_extra_fields(row_keys: Tuple, columns: Tuple) -> None:
+        """
+        Check for extra fields in the row that are not in the columns
+        """
+
+        extra_fields = not set(row_keys).issubset(set(columns))
+        if extra_fields:
+            raise ValueError(f"Extra fields found in row: {set(row_keys) - set(columns)}")
+
+        pass
 
     @abstractmethod
     def write_edge(self, edge: dict):
