939769ed8180cf352d830c2df50cdf86cbf19c5c
Patrick Golden <ptgolden@email.unc.com>
2025-01-10 12:19:19 -0500

    Add tests to CSVReader
    Also remove unnecessary `files=[]` calls, since that is the default as
    of eaff691.

--- tests/unit/test_csvreader.py
+++ tests/unit/test_csvreader.py
@@ -1,9 +1,10 @@
+from io import StringIO
 from pathlib import Path
 
 import pytest
-
 from koza.io.reader.csv_reader import CSVReader
-from koza.model.config.source_config import FieldType, CSVReaderConfig, FormatType
+from koza.model.config.source_config import (CSVReaderConfig, FieldType,
+                                             FormatType)
 
 test_file = Path(__file__).parent.parent / 'resources' / 'source-files' / 'string.tsv'
 tsv_with_footer = Path(__file__).parent.parent / 'resources' / 'source-files' / 'tsv-with-footer.tsv'
@@ -27,7 +28,6 @@ def test_no_exceptions_in_normal_case():
     with open(test_file, 'r') as string_file:
         config = CSVReaderConfig(
             format=FormatType.csv,
-            files=[],
             field_type_map=field_type_map,
             delimiter=' ',
         )
@@ -41,7 +41,6 @@ def test_type_conversion():
     with open(test_file, 'r') as string_file:
         config = CSVReaderConfig(
             format=FormatType.csv,
-            files=[],
             field_type_map=field_type_map,
             delimiter=' ',
         )
@@ -57,7 +56,6 @@ def test_field_doesnt_exist_in_file_raises_exception():
         invalid_field_type_map = field_type_map.copy()
         invalid_field_type_map['some_field_that_doesnt_exist'] = FieldType.str
         config = CSVReaderConfig(
-            files=[],
             field_type_map=invalid_field_type_map,
             delimiter=' ',
         )
@@ -75,7 +73,6 @@ def test_field_in_file_but_not_in_config_logs_warning(caplog):
         missing_field_field_type_map = field_type_map.copy()
         del missing_field_field_type_map['combined_score']
         config = CSVReaderConfig(
-            files=[],
             field_type_map=missing_field_field_type_map,
             delimiter=' ',
         )
@@ -90,7 +87,6 @@ def test_middle_field_in_file_but_not_in_config_logs_warning(caplog):
         missing_field_field_type_map = field_type_map.copy()
         del missing_field_field_type_map['cooccurence']
         config = CSVReaderConfig(
-            files=[],
             field_type_map=missing_field_field_type_map,
             delimiter=' ',
         )
@@ -104,7 +100,6 @@ def test_middle_field_in_file_but_not_in_config_logs_warning(caplog):
 def test_no_field_map(caplog):
     with open(test_file, 'r') as string_file:
         config = CSVReaderConfig(
-            files=[],
             delimiter=' ',
         )
         reader = CSVReader(string_file, config)
@@ -119,7 +114,6 @@ def test_no_exceptions_with_footer():
     with open(tsv_with_footer, 'r') as footer_file:
         config = CSVReaderConfig(
             format=FormatType.csv,
-            files=[],
             field_type_map=field_type_map,
             delimiter=' ',
             comment_char='!!',
@@ -128,3 +122,88 @@ def test_no_exceptions_with_footer():
         # TODO actually test something
         for _ in reader:
             pass
+
+
+def test_header_delimiter():
+    test_buffer = StringIO('a/b/c\n1,2,3\n4,5,6')
+    test_buffer.name = 'teststring'
+    config = CSVReaderConfig(
+        delimiter=',',
+        header_delimiter='/',
+    )
+    reader = CSVReader(test_buffer, config)
+    assert reader.header == ["a", "b", "c"]
+    assert [row for row in reader] == [
+        {
+            "a": "1",
+            "b": "2",
+            "c": "3",
+        },
+        {
+            "a": "4",
+            "b": "5",
+            "c": "6",
+        },
+    ]
+
+
+def test_header_prefix():
+    test_buffer = StringIO("# a|b|c")
+    test_buffer.name = 'teststring'
+    config = CSVReaderConfig(
+        header_delimiter='|',
+        header_prefix="# ",
+    )
+    reader = CSVReader(test_buffer, config)
+    assert reader.header == ["a", "b", "c"]
+
+
+def test_header_skip_lines():
+    test_buffer = StringIO("skipped line 1\nskipped line 2\na,b,c")
+    test_buffer.name = 'teststring'
+    config = CSVReaderConfig(
+        header_mode=2,
+        delimiter=',',
+    )
+    reader = CSVReader(test_buffer, config)
+    assert reader.header == ["a", "b", "c"]
+
+
+def test_default_config():
+    test_buffer = StringIO("a\tb\tc\n1\t2\t3\n4\t5\t6")
+    test_buffer.name = 'teststring'
+    config = CSVReaderConfig()
+    reader = CSVReader(test_buffer, config)
+    assert [row for row in reader] == [
+        {
+            "a": "1",
+            "b": "2",
+            "c": "3",
+        },
+        {
+            "a": "4",
+            "b": "5",
+            "c": "6",
+        },
+    ]
+
+
+def test_header_with_leading_comments():
+    test_buffer = StringIO("# comment 1\n#comment 2\na,b,c")
+    test_buffer.name = 'teststring'
+    config = CSVReaderConfig(
+        comment_char='#',
+        delimiter=',',
+    )
+    reader = CSVReader(test_buffer, config)
+    assert reader.header == ["a", "b", "c"]
+
+
+def test_header_with_blank_lines():
+    test_buffer = StringIO("\n\n\n\na,b,c")
+    test_buffer.name = 'teststring'
+    config = CSVReaderConfig(
+        delimiter=',',
+    )
+    reader = CSVReader(test_buffer, config)
+    assert reader.header == ["a", "b", "c"]
