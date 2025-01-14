1c2d8fb2803c2b6337b3b806056fb32c836d2b98
Patrick Golden <ptgolden@email.unc.com>
2025-01-14 11:18:34 -0500

    Move CLI to new API

--- src/koza/main.py
+++ src/koza/main.py
@@ -1,15 +1,19 @@
 #!/usr/bin/env python3
 """CLI for Koza - wraps the koza library to provide a command line interface"""
 
+import sys
 from pathlib import Path
-from typing import Optional
-
-from koza.cli_utils import transform_source, validate_file, split_file
-from koza.model.config.source_config import FormatType, OutputFormat
+from typing import Annotated, Optional
 
 import typer
+from loguru import logger
 
-typer_app = typer.Typer()
+from koza.model.formats import OutputFormat
+from koza.runner import KozaRunner
+
+typer_app = typer.Typer(
+    no_args_is_help=True,
+)
 
 
 @typer_app.callback(invoke_without_command=True)
@@ -23,61 +27,80 @@ def callback(version: Optional[bool] = typer.Option(None, "--version", is_eager=
 
 @typer_app.command()
 def transform(
-    source: str = typer.Option(..., help="Source metadata file"),
-    output_dir: str = typer.Option("./output", help="Path to output directory"),
-    output_format: OutputFormat = typer.Option("tsv", help="Output format"),
-    global_table: str = typer.Option(None, help="Path to global translation table"),
-    local_table: str = typer.Option(None, help="Path to local translation table"),
-    schema: str = typer.Option(None, help="Path to schema YAML for validation in writer"),
-    row_limit: int = typer.Option(None, help="Number of rows to process (if skipped, processes entire source file)"),
-    verbose: Optional[bool] = typer.Option(None, "--debug/--quiet"),
-    log: bool = typer.Option(False, help="Optional log mode - set true to save output to ./logs"),
+    configuration_yaml: Annotated[
+        str,
+        typer.Argument(help="Configuration YAML file"),
+    ],
+    output_dir: Annotated[
+        str,
+        typer.Option("--output-dir", "-o", help="Path to output directory"),
+    ] = "./output",
+    output_format: Annotated[
+        OutputFormat,
+        typer.Option("--output-format", "-f", help="Output format"),
+    ] = OutputFormat.tsv,
+    row_limit: Annotated[
+        int,
+        typer.Option("--limit", "-n", help="Number of rows to process (if skipped, processes entire source file)"),
+    ] = 0,
+    quiet: Annotated[
+        bool,
+        typer.Option("--quiet", "-q", help="Disable log output"),
+    ] = False,
 ) -> None:
     """Transform a source file"""
+    logger.remove()
+
     output_path = Path(output_dir)
+
     if output_path.exists() and not output_path.is_dir():
         raise NotADirectoryError(f"{output_dir} is not a directory")
     elif not output_path.exists():
         output_path.mkdir(parents=True)
-    transform_source(
-        source,
-        output_dir,
-        output_format,
-        global_table,
-        local_table,
-        schema,
-        node_type=None,
-        edge_type=None,
+
+    if not quiet:
+        prompt = "{time:YYYY-MM-DD HH:mm:ss.SSS} | <level>{level}</level> | <level>{message}</level>"
+        logger.add(sys.stderr, format=prompt, colorize=True)
+
+    # FIXME: Verbosity, logging
+    config, runner = KozaRunner.from_config_file(
+        configuration_yaml,
+        output_dir=output_dir,
+        output_format=output_format,
         row_limit=row_limit,
-        verbose=verbose,
-        log=log,
     )
 
+    logger.info(f"Running transform for {config.name} with output to `{output_dir}`")
 
-@typer_app.command()
-def validate(
-    file: str = typer.Option(..., help="Path or url to the source file"),
-    format: FormatType = FormatType.csv,
-    delimiter: str = ",",
-    header_delimiter: str = None,
-    skip_blank_lines: bool = True,
-) -> None:
-    """Validate a source file"""
-    validate_file(file, format, delimiter, header_delimiter, skip_blank_lines)
+    runner.run()
+
+    logger.info(f"Finished transform for {config.name}")
 
 
-@typer_app.command()
-def split(
-    file: str = typer.Argument(..., help="Path to the source kgx file to be split"),
-    fields: str = typer.Argument(..., help="Comma separated list of fields to split on"),
-    remove_prefixes: bool = typer.Option(
-        False,
-        help="Remove prefixes from the file names for values from the specified fields. (e.g, NCBIGene:9606 becomes 9606",
-    ),
-    output_dir: str = typer.Option(default="output", help="Path to output directory"),
-):
-    """Split a file by fields"""
-    split_file(file, fields, remove_prefixes=remove_prefixes, output_dir=output_dir)
+# @typer_app.command()
+# def validate(
+#     file: str = typer.Option(..., help="Path or url to the source file"),
+#     format: InputFormat = InputFormat.csv,
+#     delimiter: str = ",",
+#     header_delimiter: str = None,
+#     skip_blank_lines: bool = True,
+# ) -> None:
+#     """Validate a source file"""
+#     validate_file(file, format, delimiter, header_delimiter, skip_blank_lines)
+#
+#
+# @typer_app.command()
+# def split(
+#     file: str = typer.Argument(..., help="Path to the source kgx file to be split"),
+#     fields: str = typer.Argument(..., help="Comma separated list of fields to split on"),
+#     remove_prefixes: bool = typer.Option(
+#         False,
+#         help="Remove prefixes from the file names for values from the specified fields. (e.g, NCBIGene:9606 becomes 9606",
+#     ),
+#     output_dir: str = typer.Option(default="output", help="Path to output directory"),
+# ):
+#     """Split a file by fields"""
+#     split_file(file, fields, remove_prefixes=remove_prefixes, output_dir=output_dir)
 
 
 if __name__ == "__main__":
