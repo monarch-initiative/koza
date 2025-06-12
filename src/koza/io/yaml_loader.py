02085dde1c6439e7418799f2cc16a56da26dbfb2
Patrick Golden <ptgolden@email.unc.edu>
2025-06-12 13:40:58 -0400

    Refactor and fix types on UniqueIncludeYamlLoader

--- src/koza/io/yaml_loader.py
+++ src/koza/io/yaml_loader.py
@@ -11,8 +11,6 @@ Include loader based on: https://matthewpburruss.com/post/yaml/ and
                          https://stackoverflow.com/a/9577670
 """
 
-from typing import IO, Union
-
 import yaml
 from yaml import SafeLoader
 from yaml.constructor import ConstructorError
@@ -27,7 +25,7 @@ class UniqueIncludeLoader(SafeLoader):
     - an '!include' tag for importing other yaml files
     """
 
-    def unique_construct_mapping(self, node: yaml.Node, deep=False):
+    def unique_construct_mapping(self, node: yaml.MappingNode, deep=False):
         mapping = []
         for key_node, value_node in node.value:
             key = self.construct_object(key_node, deep=deep)
@@ -36,14 +34,14 @@ class UniqueIncludeLoader(SafeLoader):
             mapping.append(key)
         return super().construct_mapping(node, deep)
 
-    def include_constructor(self, node: yaml.Node) -> Union[str, IO[str]]:
-        """
-        Opens some resource (local or remote file) that appears after an !include tag
-        """
-        resource = open_resource(self.construct_scalar(node))
-        if isinstance(resource, tuple):
-            raise ValueError("Cannot load yaml from archive files")
-        return yaml.load(resource.reader, Loader=UniqueIncludeLoader)
+def include_constructor(loader: yaml.SafeLoader, node: yaml.ScalarNode):
+    """
+    Opens some resource (local or remote file) that appears after an !include tag
+    """
+    filename = loader.construct_scalar(node)
+    resource = open_resource(filename)
+    if isinstance(resource, tuple):
+        raise ValueError("Cannot load yaml from archive files")
+    return yaml.load(resource.reader, Loader=UniqueIncludeLoader)  # noqa: S506
 
-
-yaml.add_constructor("!include", UniqueIncludeLoader.include_constructor, UniqueIncludeLoader)
+UniqueIncludeLoader.add_constructor("!include", include_constructor)
