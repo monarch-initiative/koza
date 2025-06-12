e870fad2d3b7aca864c02f818bbe203e3e324274
Patrick Golden <ptgolden@email.unc.edu>
2025-06-12 13:02:03 -0400

    Linting log_utils.py

--- src/koza/utils/log_utils.py
+++ src/koza/utils/log_utils.py
@@ -2,16 +2,19 @@ import sys
 
 import loguru
 
-LOGURU_FORMAT = "<green>{time:YYYY-MM-DD_HH:mm:ss}</green> | <level>{level: <8}</level> | <cyan>{name: <16}</cyan> | <level>{message}</level>"
+LOGURU_FORMAT = (
+    "<green>{time:YYYY-MM-DD_HH:mm:ss}</green> | <level>{level: <8}</level> | "
+    "<cyan>{name: <16}</cyan> | <level>{message}</level>"
+)
 LOGURU_FORMAT_SIMPLE = "{time:YYYY-MM-DD_HH:mm:ss} | {level: <8} | {name: <16} | {message}"
 
 
-def get_logger(name: str = None, verbose: bool = None):
+def get_logger(name: str | None = None, verbose: bool | None = None):
     logger = loguru.logger
     logger.remove()
     logger.add(
         sink=sys.stderr,
-        level="INFO" if (verbose is None) else "DEBUG" if (verbose == True) else "WARNING",
+        level="INFO" if (verbose is None) else "DEBUG" if verbose else "WARNING",
         format=LOGURU_FORMAT,
         colorize=True,
     )
